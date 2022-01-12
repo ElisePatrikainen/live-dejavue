18fac34638ac60775c1da51fbe940968b8b2b635
ElisePatrikainen <elise.patrikainen@gmail.com>
2022-01-12 14:50:02 +0100

    second commit

--- app.vue
+++ app.vue
@@ -1,14 +1,22 @@
 <script setup>
-const receivedData = ref('')
+const newData = ref('')
+const { data } = await useFetch('/api/test')
+const currentData = ref(JSON.parse(data.value).description)
 const test = async () => {
-  receivedData.value = await $fetch('/api/test', { method: 'POST', body: { some: 'json' } })
+  await $fetch('/api/test', { method: 'POST', body: {description: newData.value}})
+  // todo: handle errors
+  currentData.value = newData.value
+  console.log(currentData.value)
+  newData.value = ''
 }
 </script>
 
 
 <template>
   <div>
+  {{newData}}
+  <input v-model="newData" />
   <button @click="test">Test</button>
-  {{receivedData}}
+  {{currentData}}
   </div>
 </template>
--- server/api/test.ts
+++ server/api/test.ts
@@ -1,7 +1,16 @@
 import { useBody } from 'h3'
+import fs from 'fs'
+
+const test = async (req) => {
+    let body = await useBody(req)
+    console.log('OKKKKK')
+    fs.writeFileSync('data.json', JSON.stringify(body))
+}
 
 export default async (req, res) => {
-    let body = await useBody(req)
-
-    return body
+    console.log("Yo")
+    switch (req.method) {
+        case 'GET': console.log("YoKK"); return fs.readFileSync('data.json')
+        case 'POST': test(req); return 'OK'
+    } 
 }
